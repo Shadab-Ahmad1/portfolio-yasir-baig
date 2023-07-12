aa9863ac68bf672c9c867b40f0154ea6190a1ad7
shadab <ashadab124@gmail.com>
2023-07-12 19:20:18 +0500

    first commit

--- src/app/main-view/main-view.component.ts
+++ src/app/main-view/main-view.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, HostListener, OnInit , } from '@angular/core';
+import { Router,NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 @Component({
   selector: 'app-main-view',
   templateUrl: './main-view.component.html',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainViewComponent implements OnInit {
 
-  constructor() { }
+  isDesktop: boolean = true;
+  
+  @HostListener('window:resize', ['$event'])
+  onWindowResize(event: Event) {
+    this.checkViewport();
+  }
+  isOpen = false;
+  isOpenProfile = false;
+  activeRoute: string | undefined;
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.router.events
+      .pipe(filter((event:any) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.setActiveRoute(event.urlAfterRedirects);
+      });
+      this.checkViewport();
+  }
+  setActiveRoute(url: string) {
+    if (url.includes('/portfolioDetails') || url.includes('?selectedItemId')) {
+      this.activeRoute = 'UI/UX';
+    } else {
+      this.activeRoute = url.slice(1);
+    }
+  }
+  
+  isPortfolioActive(): boolean {
+    const currentUrl = this.router.url;
+    return currentUrl.includes('portfolio') || currentUrl.includes('UI/UX') || currentUrl.includes('portfolioDetails') || currentUrl.includes('graphic-designing');
+  }
+  toggleSidebar() {
+    this.isOpen = !this.isOpen;
+  }
+  toggleProfileSidebar() {
+    this.isOpenProfile = !this.isOpenProfile;
+  }
+  checkViewport() {
+    this.isDesktop = window.innerWidth >= 700;
   }
 
 }
